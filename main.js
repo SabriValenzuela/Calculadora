b9d783bcb1a1a862af40ff1c95efa400eccc7786
Sabrina Valenzuela <sabrina.valenzuela@gmail.com>
2023-06-11 22:16:40 -0400

    Primera parte

---
+++ main.js
@@ -0,0 +1,8 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".botones");
+
+botones.forEach(botones => ) {
+        botones.addEventListener("click , ()=>" {
+            console.log(botones.textContent);
+
+        })
