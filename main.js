2fd71d6d464535eb8bd169ad7094b54607b02995
Sabrina Valenzuela <sabrina.valenzuela@gmail.com>
2023-06-21 00:36:18 +0000

    version final calculadora

--- main.js
+++
@@ -1,8 +0,0 @@
-const pantalla = document.querySelector(".pantalla");
-const botones = document.querySelectorAll(".botones");
-
-botones.forEach(botones => ) {
-        botones.addEventListener("click , ()=>" {
-            console.log(botones.textContent);
-
-        })
